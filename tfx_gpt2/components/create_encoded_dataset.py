be24c31c3359dee32d6e11a297782ce09895ee21
Steven <stevenmi@live.de>
2020-09-24 21:16:16 +0200

    Update create_encoded_dataset.py

--- tfx_gpt2/components/create_encoded_dataset.py
+++ tfx_gpt2/components/create_encoded_dataset.py
@@ -31,10 +31,11 @@ class Executor(base_executor.BaseExecutor):
         encoding = exec_properties["encoding"]
         merged_text_dir = get_single_uri(input_dict["merged_text_dir"])
         encoding_dir = get_single_uri(input_dict["encoding_dir"])
+        end_token = exec_properties["end_token"]
 
         logging.info('Reading files')
         enc = encoder.get_encoder(encoding_dir)
-        chunks = load_dataset(enc, merged_text_dir, encoding=encoding)
+        chunks = load_dataset(enc, merged_text_dir, encoding=encoding, end_token=end_token)
 
         dataset_path = os.path.join(get_single_uri(output_dict["dataset_dir"]), "dataset.npz")
         logging.info('Writing', dataset_path)
@@ -44,6 +45,7 @@ class Executor(base_executor.BaseExecutor):
 class CreateEncodedDatasetSpec(types.ComponentSpec):
     PARAMETERS = {
         'encoding': ExecutionParameter(type=Text),
+        'end_token': ExecutionParameter(type=Text),
     }
 
     INPUTS = {
@@ -63,10 +65,12 @@ class CreateEncodedDataset(base_component.BaseComponent):
     def __init__(self,
                  merged_text_dir: types.Channel,
                  encoding_dir: types.Channel,
+                 end_token: Text,
                  encoding: Text = 'utf-8'):
         dataset_dir = external_input("CreateDataset")
         spec = CreateEncodedDatasetSpec(merged_text_dir=merged_text_dir,
                                         encoding_dir=encoding_dir,
                                         encoding=encoding,
+                                        end_token=end_token,
                                         dataset_dir=dataset_dir)
         super(CreateEncodedDataset, self).__init__(spec=spec)
